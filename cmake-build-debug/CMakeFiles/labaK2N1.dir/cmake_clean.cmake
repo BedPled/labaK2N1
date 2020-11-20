1570df4ca567ad4003822f187f2045e0cc370797
Alex <alexander.vischnyakov@gmail.com>
2020-11-20 21:51:23 +1000

    add Less function

---
+++ cmake-build-debug/CMakeFiles/labaK2N1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labaK2N1.dir/main.cpp.o"
+  "labaK2N1.exe"
+  "labaK2N1.pdb"
+  "liblabaK2N1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labaK2N1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
